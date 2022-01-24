718003cf4a0da796865b905dfd85df145cd0eb28
arpa2001 <arpajitofficial@gmail.com>
2022-01-24 20:32:53 +0530

    Reseting
    Deleting all files

--- django_todo/todo/serializers2.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Todo
-
-
-class TodoSerializer2(serializers.ModelSerializer):
-    class Meta:
-        model = Todo
-        fields = "__all__"
